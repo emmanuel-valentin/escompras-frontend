6267994a26832ef8aa3a7af906571d47b58330d4
Alejandro <amoralest020906@hotmail.com>
2024-06-27 17:31:55 -0600

    Refactor HTML structure of app.component.html, login.component.html, and signin.component.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { FeedComponent } from './components/feed/feed.component';
 import { LoginComponent } from './components/login/login.component';
 import { SigninComponent } from './components/signin/signin.component';
+import { MyCartComponent } from './components/my-cart/my-cart.component';
 
 export const routes: Routes = [
   { "path": "", "component": FeedComponent },
   { "path": "login", "component": LoginComponent },
   { "path": "signin", "component": SigninComponent },
+  { "path": "cart", "component": MyCartComponent },
   { "path": "**", "redirectTo": "", "pathMatch": "full" }
 ];
