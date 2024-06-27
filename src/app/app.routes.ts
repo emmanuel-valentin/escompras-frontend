f1c6e63e4dc85b27666453571b847798e89950c6
Alejandro <amoralest020906@hotmail.com>
2024-06-27 01:16:33 -0600

    feat: Add login and signin components & routes to app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { FeedComponent } from './components/feed/feed.component';
+import { LoginComponent } from './components/login/login.component';
+import { SigninComponent } from './components/signin/signin.component';
 
 export const routes: Routes = [
   { "path": "", "component": FeedComponent },
+  { "path": "login", "component": LoginComponent },
+  { "path": "signin", "component": SigninComponent },
   { "path": "**", "redirectTo": "", "pathMatch": "full" }
 ];
