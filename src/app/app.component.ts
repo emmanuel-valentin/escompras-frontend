6fb040bee16a66c3884273222fde6806b038ab7a
Alejandro <amoralest020906@hotmail.com>
2024-06-28 13:51:51 -0600

    refactor: Add footer component to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from "./components/menu/menu.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, MenuComponent]
+  imports: [RouterOutlet, MenuComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'ESCOMpras-frontend';
